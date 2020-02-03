9bff36e7d330ef982dbc3ab99175486029581736
rajan.opallabs <rajan.opallabs@gmail.com>
2020-02-03 12:47:25 +0530

    How to create object,call methods etc.

---
+++ src/FirstDemo.java
@@ -0,0 +1,17 @@
+public class FirstDemo {
+
+    void showData(){
+
+        System.out.printf("Hello");
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        FirstDemo demo=new FirstDemo();
+        demo.showData();
+    }
+
+}
