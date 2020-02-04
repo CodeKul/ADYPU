065b6d1c2df47229c1409c6274c41c70e979aa03
rajan.opallabs <rajan.opallabs@gmail.com>
2020-02-04 15:36:54 +0530

    twoDArray completed.

--- src/arrays/ArrayDemo.java
+++ src/arrays/ArrayDemo.java
@@ -14,11 +14,41 @@ public class ArrayDemo {
         }
     }
 
-    public static void main(String[] args) {
-        ArrayDemo demo=new ArrayDemo();
-        demo.oneDArray();
+  /*  1 2 3
+    4 5 6
+    7 8 9*/
+
+    void twoDArray(){
+        int array[][]={{1,2,3},{4,5,6},{7,8,9}};
+
+        for(int i=0;i<array.length;i++){
+            for (int j=array.length-1;j>=0;j--){
+
+                System.out.print(" "+array[i][j]);
+
+            }
+            System.out.println();
+        }
     }
 
-
-
+    public static void main(String[] args) {
+        ArrayDemo demo=new ArrayDemo();
+        demo.twoDArray();
+    }
 }
+
+/*1 2 3
+  4 5 6
+  7 8 9
+
+   3 2 1
+   6 5 4
+   9 8 7
+
+   02 01 00
+   12 11 10
+   22 21 20
+
+00 01 02
+10 11 12
+20 21 22*/
