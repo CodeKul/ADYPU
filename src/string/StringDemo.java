2b997055dacf64053efb907143b5835bda914cdd
rajan.opallabs <rajan.opallabs@gmail.com>
2020-02-07 11:12:01 +0530

    String completed.

---
+++ src/string/StringDemo.java
@@ -0,0 +1,31 @@
+package string;
+
+public class StringDemo {
+    public static void main(String[] args) {
+
+        String s="java";
+        String s1="java";
+        String s2=new String("java");
+     /*   System.out.println("hash of s : "+s.hashCode());
+        System.out.println("hash of s1 : "+s1.hashCode());
+        s=s.concat(" Training");
+        System.out.println("s : "+s+"\nhash :"+s.hashCode());
+*//*   System.out.println("hash of s : "+s.hashCode());
+        System.out.println("hash of s1 : "+s1.hashCode());
+        s=s.concat(" Training");
+        System.out.println("s : "+s+"\nhash :"+s.hashCode());
+*/
+
+        System.out.println(s.substring(0,2));
+       /* if (s.equals(s2)){
+            System.out.println(true);
+        }
+        else
+            System.out.println(false);
+*/
+/*        System.out.println("location of s : "+s.hashCode());
+        System.out.println("location of s1 : "+s1.hashCode());*/
+
+    }
+
+}
